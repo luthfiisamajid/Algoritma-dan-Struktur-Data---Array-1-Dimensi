815e71c01f6f073bbb84e0de576aaf03c39b1c05
Gofur Riswanto <luthfiisamajid@gmail.com>
2024-09-23 11:30:50 +0700

    first commit

---
+++ luthfi_1.py
@@ -0,0 +1,4 @@
+nilai_Mahasiswa = [90,78,80,79]
+print(nilai_Mahasiswa)
+rata_rata = sum(nilai_Mahasiswa)/len(nilai_Mahasiswa)
+print(rata_rata)
---
+++ tes.py
@@ -0,0 +1,6 @@
+A = [21,23,4]
+B = ["Alpukat","Apel","jambu"]
+C = [2.34,3.45,4.55]
+print(A[2])
+print(B[1])
+print(C[0])
