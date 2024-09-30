c4ff55caae8db06614a34a00fdb15f6510211529
Luthfi Isa Majid <107035276+luthfiisamajid@users.noreply.github.com>
2024-09-30 09:25:05 +0700

    Update luthfi_1.py

--- luthfi_1.py
+++ luthfi_1.py
@@ -1,4 +1,4 @@
 nilai_Mahasiswa = [90,78,80,79]
-print(nilai_Mahasiswa)
+print(f"Nilai Mahasiswa : {nilai_Mahasiswa}")
 rata_rata = sum(nilai_Mahasiswa)/len(nilai_Mahasiswa)
-print(rata_rata)
+print(f"Rata-Rata Nilai Mahasiswa adalah {rata_rata}")
